2d62e9c695606c0762fabfd7c5fc8096bb6b5e62
Rajkishore Behera <rajkishorebehera358@gmail.com>
2025-04-06 13:56:28 +0530

    tempCode updated

--- Basic-Web-Development/JavaScript/WorldBestJSCourse-master/tempCodeRunnerFile.js
+++ Basic-Web-Development/JavaScript/WorldBestJSCourse-master/tempCodeRunnerFile.js
@@ -1,1 +1,73 @@
-var result = sum(5, 5);
+//* ====================================
+//* Values and Variable in JavaScript
+//* ====================================
+
+console.log("welcome to my paradise");
+
+var rollNo = 7023;
+console.log(rollNo);
+
+var name = "RAJ";
+console.log(name);
+
+//* ============================
+//* Data Types Section
+//* ============================
+
+var num = 5;
+console.log(num);
+console.log(typeof num);
+
+var myName = "RAJ";
+console.log(myName);
+console.log(typeof myName);
+
+var available = false;
+console.log(available);
+console.log(typeof available);
+
+var undefinedVariable;
+console.log(undefinedVariable);
+console.log(typeof undefinedVariable);
+
+var nullValue = null;
+console.log(nullValue);
+console.log(typeof nullValue);
+
+const bigNumber = 1234567890123456789012345678901234567890n;
+console.log(bigNumber);
+console.log(typeof bigNumber);
+
+const mySymbol = Symbol("description");
+console.log(mySymbol);
+console.log(typeof mySymbol);
+
+//* ========== parseInt & parseFloat Section ==========
+
+const myString = "42.5";
+const myNumber = parseInt(myString);
+console.log(myNumber);
+console.log(typeof myNumber);
+
+const myStrings = "42.5";
+const myNumbers = parseFloat(myStrings);
+console.log(myNumbers);
+console.log(typeof myNumbers);
+
+
+//* ============================
+//* Data Types Section - part 2
+//* ============================
+
+//* ===================================
+//*  EXPRESSIONS AND OPERATORS Section
+//* ====================================
+
+//* ===============================
+//* Conditional statement Section
+//* ===============================
+
+//* ===============================
+//* Function in JavaScript
+//* ==============================
+
