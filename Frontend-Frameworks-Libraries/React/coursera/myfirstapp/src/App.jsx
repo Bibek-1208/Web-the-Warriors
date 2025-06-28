efa58d84a31cac96a851af48b8b379bdf9dc7ac1
Bablu08 <rajkishorebehera358@gmail.com>
2025-06-28 13:16:14 +0530

    update first react app

--- Frontend-Frameworks-Libraries/React/coursera/myfirstapp/src/App.jsx
+++ Frontend-Frameworks-Libraries/React/coursera/myfirstapp/src/App.jsx
@@ -1,9 +1,11 @@
-function App(props) {
+function App() {
   const currDate = new Date();
+  const myName = ["Rathord", "Raj", "max", "Herry"];
 
   return (
     <div>
       <h1>Hello, world!</h1>
+      <h2>I am {myName.at(3)}!</h2>
       <h2>The time now is {currDate.toLocaleTimeString()}.</h2>
     </div>
   );
