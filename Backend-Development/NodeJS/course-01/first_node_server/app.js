7433e4f660f38109051bc6e872474f0a5405289d
Bablu08 <rajkishorebehera358@gmail.com>
2025-08-21 18:31:26 +0530

    add new files

--- Backend-Development/NodeJS/course-01/first_node_server/app.js
+++ Backend-Development/NodeJS/course-01/first_node_server/app.js
@@ -2,12 +2,39 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
+
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><h1>Wellcome to index page</h1></body>');
+        res.write('</html>');
+        return res.end();
+
+    } else if (req.url === '/home') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html>');
+        res.write('<head><title>Home Page</title></head>');
+        res.write('<body><h1>Welcome to the Home Page</h1></body>');
+        res.write('</html>');
+        return res.end();
+
+    } else if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html>');
+        res.write('<head><title>About Page</title></head>');
+        res.write('<body><h1>About Us</h1></body>');
+        res.write('</html>');
+        return res.end();
+        
+    } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html>');
+        res.write('<head><title>404</title></head>');
+        res.write('<body><h1>Page not found!</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
 });
 
 const PORT = 3000;
