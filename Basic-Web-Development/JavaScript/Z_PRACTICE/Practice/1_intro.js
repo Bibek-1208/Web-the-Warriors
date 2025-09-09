80b6b31aa8a20f8663ac9963b1e7888a4b4560c5
Bablu08 <rajkishorebehera358@gmail.com>
2025-09-09 14:25:48 +0530

    practice file

---
+++ Basic-Web-Development/JavaScript/Z_PRACTICE/Practice/1_intro.js
@@ -0,0 +1,74 @@
+// -------------- JavaScript Revision Roadmap -------------- \\
+
+
+// -------------- 1. Basics & First Steps -------------- \\
+
+// Print "Hello World"
+console.log("Hello World!");
+
+// Comments (single-line, multi-line)
+
+
+// Variables (var, let, const)
+var age = 10;
+console.log(age);
+
+let my_name = "RAJ";
+console.log(my_name);
+
+const psw = "6x#2t9@f&q7"
+console.log(psw);
+
+// Data types (string, number, boolean, null, undefined, object, symbol, bigint)
+
+// string
+let str = "i am string";
+console.log(str);
+console.log(typeof(str));
+
+// number
+let num = 34;
+console.log(num);
+console.log(typeof(num));
+
+// boolean
+let boolean = true;
+console.log(boolean);
+console.log(typeof(boolean));
+
+// null
+let Null = null;
+console.log(Null);
+console.log(typeof(Null));
+
+// undefined
+let und;
+console.log(und);
+console.log(typeof(und));
+
+// object
+let student = {
+    name: "Raj",
+    reg: 2421297023,
+    sec: "C",
+    dept: "CSE",
+    college: "NIT"
+};
+console.log(student);
+console.log(typeof(student));
+console.log("Name:", student.name);
+console.log("Reg:", student.reg);
+console.log("College:", student.college);
+
+// symbol
+let mySymbol = Symbol("description");
+console.log(mySymbol);
+console.log(typeof(mySymbol));
+
+// bigint
+let bigInt = 345678908765432234n;
+console.log(bigInt);
+console.log(typeof(bigInt));
+
+
+// -------------- 2. Operators -------------- \\
