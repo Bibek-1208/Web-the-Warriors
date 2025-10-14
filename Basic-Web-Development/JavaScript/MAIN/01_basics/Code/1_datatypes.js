d6511d7127b83859866ae9ba67de2d40382f0630
Bablu08 <rajkishorebehera358@gmail.com>
2025-10-14 16:36:04 +0530

    delete coursera f

--- Basic-Web-Development/JavaScript/MAIN/01_basics/Code/1_datatypes.js
+++ Basic-Web-Development/JavaScript/MAIN/01_basics/Code/1_datatypes.js
@@ -118,4 +118,7 @@ console.log("Java" + "Script")
 console.log("" + "")
 // console.log("" + o)
 console.log("vinod" - "thapa")
-
+console.log(true + true)
+console.log(false + false)
+console.log(true + false)
+console.log(false + true)
---
+++ Basic-Web-Development/JavaScript/MAIN/02_DSA/Code/01_array.js
@@ -0,0 +1,11 @@
+//* ======================================
+//* ARRAYS IN JAVASCRIPT
+//* =====================================
+
+//* JavaScript Array is a data structure that allows you to store and organize multiple values within a single variable.
+
+
+//* ======================================
+//*  Creating Arrays:
+//* =====================================
+
